567990ead0be3883348c88c21cf38c70d7569351
Sanjeev <snjvnxyz@gmail.com>
2024-11-07 16:24:15 +0530

    in progress

--- main.c
+++ main.c
@@ -18,23 +18,14 @@ int main(void)
 
     INIT_GPIO_PORTF_REGISTERS();
     int count = 0;
-
-    while (count < 16){
-
-            INIT_TIMER1_REGISTERS(19);
-    //        if ((count >= 8) && (count < 16)){
-    //            INIT_TIMER1_REGISTERS(12);
-    //        }
-    //        else{
-    //            INIT_TIMER1_REGISTERS(19);
-    //        }
-            count ++;
-        }
-    TIMER1_CTL_R = 0x00;
-    count = 0;
+//    INIT_TIMER1_REGISTERS(12);
     while (count < 192){
-
-        INIT_TIMER1_REGISTERS(12);
+        if (count > 48){
+            INIT_TIMER1_REGISTERS(12);
+        }
+        else{
+            INIT_TIMER1_REGISTERS(19);
+        }
 //        if ((count >= 8) && (count < 16)){
 //            INIT_TIMER1_REGISTERS(12);
 //        }
@@ -68,7 +59,7 @@ void INIT_GPIO_PORTF_REGISTERS(){
     GPIO_PORTF_CR_R = 0x1F;             /* make PORTF configurable */
     GPIO_PORTF_DEN_R = 0x0F;            /* set PORTF pins 4 : 0 pins */
     GPIO_PORTF_DIR_R = 0x0E;            /*  */
-    GPIO_PORTF_PUR_R = 0x02;            /* PORTF0 and PORTF4 are pulled up */
+    GPIO_PORTF_PUR_R = 0x0E;            /* PORTF0 and PORTF4 are pulled up */
     GPIO_PORTF_AFSEL_R = 0x08; // Select PORTF3 (Green LED) for Alternate Function:
                                // Green LED not driven by GPIO_PORTF_DATA_R but instead by T1CCP1 (PWM Signal from GPT1)
     GPIO_PORTF_PCTL_R = 0x00007000; // connects the PWM output of GPTM1 to PORTF3
