ac7de03f30ff96069456123cc5b04ce0cf24e79e
Sanjeev <snjvnxyz@gmail.com>
2024-11-28 22:35:01 +0530

    removed unnecessary commented code

--- main.c
+++ main.c
@@ -77,38 +77,10 @@ int main(void)
                             continue;
                         }
                     }
-//                    else if (state == CLOCK_PAUSE){
-//                        continue;
-//                    }
+
                     if( (bytestream[byte_index] << bit_index) & bitsel ){
                         INIT_TIMER1_REGISTERS(8);
-//                        if(state == CLOCK_RUN){
-//                            INIT_TIMER1_REGISTERS(8);
-//                        }
-//                        else if(state == CLOCK_SET_HR){
-//                            if ((byte_index%3) == 1){
-//                                INIT_TIMER1_REGISTERS(8);
-//                            }
-//                            else{
-//                                INIT_TIMER1_REGISTERS(12);
-//                            }
-//                        }
-//                        else if(state == CLOCK_SET_MIN){
-//                            if ((byte_index%3) == 2){
-//                                INIT_TIMER1_REGISTERS(8);
-//                            }
-//                            else{
-//                                INIT_TIMER1_REGISTERS(12);
-//                            }
-//                        }
-//                        else if(state == CLOCK_SET_SEC){
-//                            if ((byte_index%3) == 0){
-//                                INIT_TIMER1_REGISTERS(8);
-//                            }
-//                            else{
-//                                INIT_TIMER1_REGISTERS(12);
-//                            }
-//                        }
+
                     }
 
                     else{
